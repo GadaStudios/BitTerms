caf91eeb9f2684a46f03606c559abbb305d8f531
WebSculptor <abdullahisalihuinusa@gmail.com>
2025-09-14 21:23:13 +0100

    feat: updated recent component

--- app/(routes)/suggest/_components/recent.comp.tsx
+++ app/(routes)/suggest/_components/recent.comp.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import React from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
 
 import { useGlobal } from "@/app/provider";
 import { Badge } from "@/components/ui/badge";
-import { SearchFilterProps, searchFilterSchema } from "@/lib/validators";
 import Wrapper from "@/components/shared/wrapper";
 import { Skeleton } from "@/components/ui/skeleton";
 
@@ -15,40 +11,6 @@ export const RecentlyAddedComp = () => {
   const {
     terms: { data, isFetching },
   } = useGlobal();
-
-  const router = useRouter();
-
-  // get term from current URL on mount
-  const [activeTerm, setActiveTerm] = React.useState<string>("");
-
-  React.useEffect(() => {
-    if (typeof window !== "undefined") {
-      const params = new URLSearchParams(window.location.search);
-      setActiveTerm(params.get("term") ?? "");
-    }
-  }, []);
-
-  const form = useForm<SearchFilterProps>({
-    resolver: zodResolver(searchFilterSchema),
-    defaultValues: { term: activeTerm ?? "" },
-  });
-
-  function handleChange(value: string) {
-    form.setValue("term", value, { shouldValidate: true });
-
-    const params = new URLSearchParams(
-      typeof window !== "undefined" ? window.location.search : "",
-    );
-
-    if (!value.trim()) {
-      params.delete("term");
-    } else {
-      params.set("term", value.trim());
-    }
-
-    router.push(`/terms?${params.toString()}`, { scroll: false });
-    setActiveTerm(value.trim());
-  }
 
   if (!data.length) return null;
 
@@ -69,7 +31,6 @@ export const RecentlyAddedComp = () => {
               .map((tag) => (
                 <Badge
                   key={tag._id}
-                  onClick={() => handleChange(tag.name as string)}
                   className="text-foreground cursor-pointer bg-[#F9FDE5] px-2 py-1 text-xs font-normal sm:px-4 sm:text-sm"
                 >
                   {tag.name}
